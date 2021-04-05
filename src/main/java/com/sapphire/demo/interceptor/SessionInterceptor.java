b42ef9ca0d07f3b333f4faea5e3dedf6e9382872
柳李逸 <806990525@qq.com>
2021-04-05 15:07:51 +0800

    增加了图标，修复了BUG

--- src/main/java/com/sapphire/demo/interceptor/SessionInterceptor.java
+++ src/main/java/com/sapphire/demo/interceptor/SessionInterceptor.java
@@ -31,7 +31,8 @@ public class SessionInterceptor implements HandlerInterceptor {
     
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Cookie[] cookies = request.getCookies();
+    	Cookie[] cookies = request.getCookies();
+    	
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("token")) {
@@ -60,12 +61,12 @@ public class SessionInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
-    	modelAndView.addObject("loginURL", href);
+    	request.getSession().setAttribute("loginURL", href);
+  
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
+    	
     }
 }
